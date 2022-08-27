8cb3d1e2aebe488b667f8b06f487de9ba42c11ce
yondy <yondy@126.com>
2022-08-28 02:11:04 +0800

    支持按文件树分组显示备注列表

--- src/main/java/com/github/wenzewoo/coderemark/treeView/CodeRemarkFavoriteListProvider.java
+++ src/main/java/com/github/wenzewoo/coderemark/treeView/CodeRemarkFavoriteListProvider.java
@@ -29,6 +29,7 @@ import com.github.wenzewoo.coderemark.listener.CodeRemarkListener;
 import com.github.wenzewoo.coderemark.repository.CodeRemarkRepositoryFactory;
 import com.github.wenzewoo.coderemark.toolkit.StringUtils;
 import com.intellij.ide.favoritesTreeView.AbstractFavoritesListProvider;
+import com.intellij.ide.favoritesTreeView.FavoritesListNode;
 import com.intellij.ide.favoritesTreeView.FavoritesManager;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
@@ -61,6 +62,8 @@ public class CodeRemarkFavoriteListProvider extends AbstractFavoritesListProvide
         fileRemark.setLineNumber(1);
         fileRemark.setText("");
         AbstractTreeNode<CodeRemark> treeNode = new AbstractTreeNode<>(myProject, fileRemark) {
+            private List<AbstractTreeNode<CodeRemark>> children = new ArrayList<>();
+
             @Override
             protected void update(@NotNull final PresentationData presentation) {
                 presentation.setIcon(CodeRemark.getIcon());
@@ -81,7 +84,7 @@ public class CodeRemarkFavoriteListProvider extends AbstractFavoritesListProvide
 
             @Override
             public @NotNull Collection<? extends AbstractTreeNode<?>> getChildren() {
-                return Collections.emptyList();
+                return children;
             }
         };
         treeNode.setParent(parent);
@@ -90,51 +93,24 @@ public class CodeRemarkFavoriteListProvider extends AbstractFavoritesListProvide
         return treeNode;
     }
 
-    private void createRemarkNode(List<AbstractTreeNode<CodeRemark>> childNode, AbstractTreeNode parent, CodeRemark codeRemark) {
-
-        AbstractTreeNode<CodeRemark> treeNode = new AbstractTreeNode<>(myProject, codeRemark) {
-            @Override
-            protected void update(@NotNull final PresentationData presentation) {
-                presentation.setIcon(CodeRemark.getIcon());
-                String tip = codeRemark.getFileName() + ":" + (codeRemark.getLineNumber() + 1);
-                presentation.setTooltip(tip);
-                presentation.setPresentableText(codeRemark.getText());
-            }
-
-            @Override
-            public boolean canNavigate() {
-                return codeRemark.canNavigate();
-            }
-
-            @Override
-            public boolean canNavigateToSource() {
-                return codeRemark.canNavigateToSource();
-            }
-
-            @Override
-            public void navigate(final boolean requestFocus) {
-                codeRemark.navigate(requestFocus);
-            }
-
-            @Override
-            public @NotNull Collection<? extends AbstractTreeNode<?>> getChildren() {
-                return Collections.emptyList();
-            }
-        };
-        treeNode.setParent(parent);
-        childNode.add(treeNode);
-    }
-
     private void updateChildren() {
         if (myProject.isDisposed()) return;
         myChildren.clear();
 
         final List<CodeRemark> codeRemarks = CodeRemarkRepositoryFactory.getInstance().list(myProject);
-        Map<String, AbstractTreeNode<CodeRemark>> nodeMap = new HashMap<>();
+        Map<String, FileNode> nodeMap = new HashMap<>();
         for (final CodeRemark codeRemark : codeRemarks) {
             String file = codeRemark.getFileName();
-//            AbstractTreeNode<CodeRemark> fileNode = nodeMap.getOrDefault(file, createFileNode(codeRemark));
-            createRemarkNode(myChildren, myNode, codeRemark);
+
+            FileNode fileNode = nodeMap.get(file);
+            if (fileNode == null) {
+                fileNode = new FileNode(myChildren, myNode, myProject, codeRemark);
+                nodeMap.put(file, fileNode);
+            }
+            RemarkNode node = new RemarkNode(myProject, codeRemark);
+            node.setParent(fileNode);
+            fileNode.addChildren(node);
+
         }
         FavoritesManager.getInstance(myProject).fireListeners(getListName(myProject));
     }
@@ -161,5 +137,88 @@ public class CodeRemarkFavoriteListProvider extends AbstractFavoritesListProvide
     @Override
     public int getWeight() {
         return 0;
+    }
+}
+
+class RemarkNode extends AbstractTreeNode<CodeRemark> {
+    private CodeRemark codeRemark;
+
+    protected RemarkNode(Project project, @NotNull CodeRemark value) {
+        super(project, value);
+        this.codeRemark = value;
+    }
+
+
+    @Override
+    protected void update(@NotNull final PresentationData presentation) {
+        presentation.setIcon(CodeRemark.getIcon());
+        String tip = codeRemark.getFileName() + ":" + (codeRemark.getLineNumber() + 1);
+        presentation.setTooltip(tip);
+        presentation.setPresentableText(codeRemark.getText());//TODO 显示行号、日期
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return codeRemark.canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return codeRemark.canNavigateToSource();
+    }
+
+    @Override
+    public void navigate(final boolean requestFocus) {
+        codeRemark.navigate(requestFocus);
+    }
+
+    @Override
+    public @NotNull Collection<? extends AbstractTreeNode<?>> getChildren() {
+        return Collections.emptyList();
+    }
+}
+
+class FileNode extends AbstractTreeNode<CodeRemark> {
+    private CodeRemark codeRemark;
+
+    private List<AbstractTreeNode<CodeRemark>> children = new ArrayList<>();
+
+    protected FileNode(List<AbstractTreeNode<CodeRemark>> list, FavoritesListNode root, Project project, @NotNull CodeRemark value) {
+        super(project, value);
+        this.codeRemark = new CodeRemark();
+        codeRemark.setFileName(value.getFileName());
+        codeRemark.setFileUrl(value.getFileUrl());
+        codeRemark.setContentHash(value.getContentHash());
+        codeRemark.setLineNumber(1);
+        codeRemark.setText("");
+        this.setParent(root);
+        list.add(this);
+    }
+
+    @Override
+    protected void update(@NotNull final PresentationData presentation) {
+        presentation.setIcon(CodeRemark.getIcon());// TODO 参照BookmarkNode显示文件类型图标
+        String tip = codeRemark.getFileName();
+        presentation.setTooltip(tip);
+        presentation.setPresentableText(tip);// TODO 参照BookmarkNode显示完整路径
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return false;
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return false;
+    }
+
+    @Override
+    public @NotNull Collection<? extends AbstractTreeNode<?>> getChildren() {
+        return children;
+    }
+
+    public void addChildren(RemarkNode remarkNode) {
+        children.add(remarkNode);// TODO 按行号排序
     }
 }
